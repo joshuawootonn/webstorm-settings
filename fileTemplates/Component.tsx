973617e1afe8f0301c3c84866b9612d668962e62
Joshua Wootonn <jwootonn@eyediagnosis.net>
2020-01-12 22:53:11 -0600

    WS-2019.3 <jwootonn@DEV-JWOOTONN Create markdown.xml, ide.general.xml, rainbow_brackets.xml, diff.xml, Component.tsx, terminal.xml, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, filetypes.xml, keymap.xml, plugin_ui.xml, watcherDefaultTasks.xml, colors.scheme.xml, notifications.xml, _@user_Darcula.icls, React.xml, Default.xml, laf.xml, workspace.xml, wsl.distributions.xml, dbnavigator.xml, vcs.xml, editor.xml, debugger.xml, studioSettings.xml, updates.xml

---
+++ fileTemplates/Component.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+
+`;
+
+export interface ComponentProps {
+
+}
+
+export const Component:React.FC<ComponentProps> = props => {
+
+    return (
+        <Wrapper >
+        
+        </Wrapper >
+    )
+}
+
+export default Component;
+
+
+
